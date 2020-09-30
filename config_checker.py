23dfeecb6041d9e99433781d76b13ad6be8d62dd
Jessica Rowell <jrowell1@gmail.com>
2020-09-30 12:52:51 -0400

    Improvements to config_checker.py

--- config_checker.py
+++ config_checker.py
@@ -16,13 +16,14 @@ def main(cfg_file):
     logger.info('Config file passed: {}'.format(cfg_file))
 
     mothur_sections = ['file_inputs', 'contigs_params', 'rename_param', 'screen_params', 'pcr_params', 'rare_seqs_param']
+    checklist = []
 
-    # This doesn't work how I expect it to...
     if set(mothur_sections) == set(config.sections()):
         logger.info("All required sections in the config file were found.")
     else:
         for section in mothur_sections:
             pair = (section, config.has_section(section))
+            checklist.append(pair[1])
             if pair[1] == True:
                 logger.info('Section found: {}'.format(section))
             else:
@@ -30,16 +31,22 @@ def main(cfg_file):
    
     if os.access(config['file_inputs']['oligos'], os.R_OK) == True:
         logger.info('{} exists and is readable'.format(config['file_inputs']['oligos']))
+        checklist.append(True)
     else:
         logger.error('{} does not exist or is not readable'.format(config['file_inputs']['oligos']))  
-        sys.exit(1)
+        checklist.append(False)
 
     if os.access(config['file_inputs']['batch_file'], os.R_OK) == True:
         logger.info('{} exists and is readable'.format(config['file_inputs']['batch_file']))
+        checklist.append(True)
     else:    
         logger.error('{} does not exist or is not readable'.format(config['file_inputs']['batch_file']))
-        sys.exit(1)
+        checklist.append(False)
 
-    logger.info("Config file checking completed.")
-    # Export config object back to main script
+    if sum(checklist) == len(checklist):
+        logger.info("Config file checking completed.")
+    else:
+        logger.error("Config file has errors. Please correct the file and run the pipeline again.")
+        sys.exit(1)
+    
     return config
